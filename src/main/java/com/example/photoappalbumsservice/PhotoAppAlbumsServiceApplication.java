26deb3397e270cee470794a8944fdfbb54043a03
cristiii <cristian.vlad10@gmail.com>
2023-02-02 15:05:51 +0200

    init

---
+++ src/main/java/com/example/photoappalbumsservice/PhotoAppAlbumsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.photoappalbumsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppAlbumsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppAlbumsServiceApplication.class, args);
+    }
+
+}
